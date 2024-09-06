cb34090f41440f3785717183aa27070fda4c91f6
Zach Guentherman <zachguentherman@gmail.com>
2024-09-06 16:02:01 -0600

    hw3 submission

---
+++ docs/hw3.md
@@ -0,0 +1,10 @@
+# Home Work 3: Basic Synchronizer
+
+## Overview
+This lab we wrote a simple synchronozer and tested it on Questa.
+
+## Deliverables
+![synchronizer screenshot](labs-and-homework-BreadboardZach/docs/assets/Screenies/SyncHW3)
+
+![synchronizer WF screenshot](labs-and-homework-BreadboardZach/docs/assets/Screenies/WaveformHW3)
+
---
+++ hdl/synchronizer/synchronizer.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity synchronizer is
+  port (
+    clk   : in    std_logic;
+    async : in    std_ulogic;
+    sync  : out   std_ulogic
+    );
+end entity synchronizer;
+
+architecture synchronizer_arch of synchronizer is
+
+signal middleman : std_ulogic;
+
+  begin
+
+process(clk)
+begin
+  if rising_edge(clk) then 
+    middleman <= async;
+  end if;
+end process;
+
+process(clk)
+begin
+  if rising_edge(clk) then 
+     sync <= middleman;
+  end if;
+end process;
+
+end architecture;
